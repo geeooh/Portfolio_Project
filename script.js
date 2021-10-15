49e72a8f7e5bb58491616af6e90fcf64c9867992
Giovannie Hernandez <Ghernandez@gvsbenefits.com>
2021-10-14 21:28:41 -0400

    added different navbar style & trounleshooting

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let element = document.querySelector(".navbar");
+/*let element = document.querySelector(".navbar");
 console.log(element);
 
 let isBlackText = element.classList.contains("black-text");
@@ -15,6 +15,27 @@ for(let i=0; i<listItems.length; i++) {
         a.classList.add("white-text");
     }
     console.log(a);
+}*/
+
+
+
+var nav = document.getElementById('nav');
+var navlinks = nav.getElementsByTagName('a');
+
+function toggleNav() {
+    (nav.classList.contains('active')) ? nav.classList.remove('active') : nav.classList.add('active');
+  }
+
+document.getElementById('nav-icon').addEventListener('click', function(e) {
+    console.log(e);
+    e.preventDefault();
+    toggleNav();
+});
+
+for(var i = 0; i < navlinks.length; i++) {
+    navlinks[i].addEventListener('click', function() {
+      toggleNav();
+  });
 }
 
 
